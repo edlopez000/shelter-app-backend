79990fca1bc3003b2d09e7616a49c44bd90c939b
Steph Peets <stephanie.peets@gmail.com>
2022-01-23 18:45:01 -0500

    initial commit

---
+++ src/main/java/com/shelterapp/backend/repositories/VolunteerRepository.java
@@ -0,0 +1,10 @@
+package com.shelterapp.backend.repositories;
+
+import com.shelterapp.backend.models.Volunteer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VolunteerRepository extends JpaRepository<Volunteer, Long> {
+
+}
