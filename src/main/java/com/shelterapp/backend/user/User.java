a6d9e1f714db4bc96f95a61e34dd35436f335226
edlopez000 <lopez.edward@outlook.com>
2022-02-06 17:58:17 -0500

    Added missing User model fields

--- src/main/java/com/shelterapp/backend/user/User.java
+++ src/main/java/com/shelterapp/backend/user/User.java
@@ -4,6 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
@@ -15,23 +17,21 @@ public class User {
     private String name;
     private String email;
     private String password;
+    private Boolean enabled = true;
 
     public User() {
     }
 
 
-    public User(Long id, String email, String password) {
+    public User(Long id, String name, String email, String password, Boolean enabled) {
         this.id = id;
+        this.name = name;
         this.email = email;
         this.password = password;
     }
 
     public Long getId() {
         return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public String getEmail() {
@@ -48,5 +48,27 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public List<String> getRoles() {
+        ArrayList<String> roles = new ArrayList<>();
+        roles.add("ROLE_USER");
+        return roles;
     }
 }
