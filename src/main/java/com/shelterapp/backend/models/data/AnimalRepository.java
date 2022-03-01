dea861e566d1332797c734fc157eda85972acad6
Steph Peets <stephanie.peets@gmail.com>
2022-03-01 00:52:42 -0500

    add in Ed's backend species logic

--- src/main/java/com/shelterapp/backend/models/data/AnimalRepository.java
+++ src/main/java/com/shelterapp/backend/models/data/AnimalRepository.java
@@ -5,8 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Repository
 @Transactional
 public interface AnimalRepository extends JpaRepository<Animal, Long> {
+    List<Animal> findBySpecies(String species);
 }
